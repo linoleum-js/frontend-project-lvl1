4e58c0f6c5ea20df9fdf4763fdf1b4b2ac7c8488
Martin <martinschulzofficial@gmail.com>
2021-07-11 11:21:38 +0500

    lesson 7

---
+++ src/games/game-gcd.js
@@ -0,0 +1,27 @@
+import readlineSync from 'readline-sync';
+
+import game from '../index.js';
+import rand from '../util.js';
+
+const gcd = (a, b) => {
+  if (!b) {
+    return a;
+  }
+
+  return gcd(b, a % b);
+};
+
+const gameEven = () => {
+  const a = rand(100);
+  const b = rand(100);
+
+  console.log(`Question: ${a} ${b}`);
+  const answer = parseInt(readlineSync.question('Your answer: '), 10);
+  const rightAnswer = gcd(a, b);
+
+  return { answer, rightAnswer };
+};
+
+const title = 'Find the greatest common divisor of given numbers.';
+
+export default () => game(gameEven, title);
